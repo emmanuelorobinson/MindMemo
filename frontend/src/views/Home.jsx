bea546d22f6b0eaf8dd6f9f106039ed2e031f237
emmanuelorobinson <manuelejeay@gmail.com>
2023-06-26 13:43:27 +0400

    layout and home template done

---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calendar from '../components/Calendar'
+import Task from '../components/Task'
+
+const Home = () => {
+  return (
+    <div>
+      <Calendar />
+      <Task />
+    </div>
+  )
+}
+
+export default Home
